8275b1b5c52ff09386d1e60f9d35767aa4b504e9
Ryan Caught <rcaught@gmail.com>
2014-06-15 15:09:37 +1000

    Partition the disk
    Using HFS+ because MSDOS doesn’t support a case-sensitive volume name
    (need volume name “Tails” for Tails persistence setup prerequisite).
    Tried exFAT but Grub doesn’t have the load modules.
    
    4GB partition is the minimum suggested by Tails, so using that.  Rest
    is defined as free space.

--- create-image.sh
+++ create-image.sh
@@ -17,6 +17,7 @@ set -x
 TAILS_ISO_URL="http://dl.amnesia.boum.org/tails/stable/tails-i386-1.0/tails-i386-1.0.iso"
 TAILS_SIG_URL="https://tails.boum.org/torrents/files/tails-i386-1.0.iso.sig"
 TAILS_KEY_URL="https://tails.boum.org/tails-signing.key"
+TAILS_PARTITION_NAME="Tails"
 
 if [ ! -d "data" ]; then
   echo "[+] Creating data/ directory..."
@@ -29,7 +30,7 @@ create_disk () {
   # This erases the TARGET disk and creates 1 FAT32 partition that is of the
   # size of the drive.
   if [ "$( uname -s )" == "Darwin" ];then
-    diskutil eraseDisk FAT32 TAILSLIVECD $TARGET_DISK
+    diskutil partitionDisk $TARGET_DISK GPT HFS+ $TAILS_PARTITION_NAME 4g Free\ Space Ignored R
   else
     echo "Currently don't support building image on this platform"
   fi
@@ -39,8 +40,8 @@ mount_disk () {
   # This mounts the USB disk and returns the mount_point of the USB disk
   local  __resultvar=$1
   if [ "$( uname -s )" == "Darwin" ];then
-    local mount_point="/Volumes/TAILSLIVECD"
-    diskutil mount -mountpoint $mount_point TAILSLIVECD
+    local mount_point="/Volumes/$TAILS_PARTITION_NAME"
+    diskutil mount -mountpoint $mount_point $TAILS_PARTITION_NAME
   else
     echo "Currently don't support building image on this platform"
     exit 1
